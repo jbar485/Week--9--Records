0eafa13061596d97fc9fd9d6e4f8181e556217e6
Student <example@example.com>
2019-12-18 11:44:29 -0800

    add songs that can be put in albums

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/album')
+require('./lib/song')
 require('pry')
 also_reload('lib/**/*.rb')
 
@@ -12,7 +13,7 @@ end
 get('/albums') do
   if params[:search]
     @albums = Album.search(params[:search])
-  else  
+  else
     @albums = Album.all
   end
   erb(:albums)
@@ -60,4 +61,34 @@ delete('/albums/:id') do
   @album.delete()
   @albums = Album.all
   erb(:albums)
+end
+
+# Get the detail for a specific song such as lyrics and songwriters.
+get('/albums/:id/songs/:song_id') do
+  @song = Song.find(params[:song_id].to_i())
+  erb(:song)
+end
+
+# Post a new song. After the song is added, Sinatra will route to the view for the album the song belongs to.
+post('/albums/:id/songs') do
+  @album = Album.find(params[:id].to_i())
+  song = Song.new(params[:song_name], @album.id, nil)
+  song.save()
+  erb(:album)
+end
+
+# Edit a song and then route back to the album view.
+patch('/albums/:id/songs/:song_id') do
+  @album = Album.find(params[:id].to_i())
+  song = Song.find(params[:song_id].to_i())
+  song.update(params[:name], @album.id)
+  erb(:album)
+end
+
+# Delete a song and then route back to the album view.
+delete('/albums/:id/songs/:song_id') do
+  song = Song.find(params[:song_id].to_i())
+  song.delete
+  @album = Album.find(params[:id].to_i())
+  erb(:album)
 end
--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -12,25 +12,25 @@ describe '#Album' do
   end
   describe('#save') do
     it("saves an album") do
-      album = Album.new("Giant Steps", nil) # nil added as second argument
+      album = Album.new("Giant Steps", nil, nil, nil, nil) # nil added as second argument
       album.save()
-      album2 = Album.new("Blue", nil) # nil added as second argument
+      album2 = Album.new("Blue", nil, nil, nil, nil) # nil added as second argument
       album2.save()
-      expect(Album.all).to(eq([album, album2]))
+      expect(Album.all).to(eq([album2, album]))
     end
   end
   describe('#==') do
     it("is the same album if it has the same attributes as another album") do
-      album = Album.new("Blue", nil)
-      album2 = Album.new("Blue", nil)
+      album = Album.new("Blue", nil, nil, nil, nil)
+      album2 = Album.new("Blue", nil, nil, nil, nil)
       expect(album).to(eq(album2))
     end
   end
   describe('.clear') do
     it("clears all albums") do
-      album = Album.new("Giant Steps", nil)
+      album = Album.new("Giant Steps", nil, nil, nil, nil)
       album.save()
-      album2 = Album.new("Blue", nil)
+      album2 = Album.new("Blue", nil, nil, nil, nil)
       album2.save()
       Album.clear()
       expect(Album.all).to(eq([]))
@@ -38,26 +38,26 @@ describe '#Album' do
   end
   describe('.find') do
     it("finds an album by id") do
-      album = Album.new("Giant Steps", nil)
+      album = Album.new("Giant Steps", nil, nil, nil, nil)
       album.save()
-      album2 = Album.new("Blue", nil)
+      album2 = Album.new("Blue", nil, nil, nil, nil)
       album2.save()
       expect(Album.find(album.id)).to(eq(album))
     end
   end
   describe('#update') do
   it("updates an album by id") do
-    album = Album.new("Giant Steps", nil)
+    album = Album.new("Giant Steps", "Jenkens", "2001", "Troy3oys", nil)
     album.save()
-    album.update("A Love Supreme")
+    album.update("A Love Supreme", "Jenkens", "2001", "Troy3oys")
     expect(album.name).to(eq("A Love Supreme"))
   end
 end
 describe('#delete') do
     it("deletes an album by id") do
-      album = Album.new("Giant Steps", nil)
+      album = Album.new("Giant Steps", nil, nil, nil, nil)
       album.save()
-      album2 = Album.new("Blue", nil)
+      album2 = Album.new("Blue", nil, nil, nil, nil)
       album2.save()
       album.delete()
       expect(Album.all).to(eq([album2]))
